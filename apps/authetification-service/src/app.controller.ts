c933a6a8dbc935bcda2e70018777b14c24e8ae14
Raytara <maxime.waechter@gmail.com>
2025-12-14 23:51:20 +0100

    feat: inscription utilisateur keyclock

--- apps/authetification-service/src/app.controller.ts
+++ apps/authetification-service/src/app.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller()
 export class AppController {
-constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @MessagePattern('hello_users_service')
   getHello(): string {
     console.log("[UsersService] Message hello reçu");
-    return "Hello world from Users Microservice"; 
+    return "Hello world from Users Microservice";
+  }
+
+  @MessagePattern('create_user')
+  async createUser(@Payload() data: CreateUserDto) {
+    return await this.appService.createUser(data);
   }
 }
--- apps/authetification-service/src/app.service.ts
+++ apps/authetification-service/src/app.service.ts
@@ -1,8 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import KcAdminClient from '@keycloak/keycloak-admin-client';
+import { ConfigService } from '@nestjs/config';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  private kcAdminClient: KcAdminClient;
+
+  constructor(private configService: ConfigService) {
+    this.kcAdminClient = new KcAdminClient({
+      baseUrl: this.configService.get<string>('KEYCLOAK_BASE_URL')!,
+      realmName: this.configService.get<string>('KEYCLOAK_REALM')!,
+    });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.kcAdminClient.auth({
+        grantType: 'client_credentials',
+        clientId: this.configService.get<string>('KEYCLOAK_CLIENT_ID_NAME')!,
+        clientSecret: this.configService.get<string>('KEYCLOAK_CLIENT_SECRET')!,
+      });
+      setInterval(async () => {
+        try {
+          await this.kcAdminClient.auth({
+            grantType: 'client_credentials',
+            clientId: this.configService.get<string>('KEYCLOAK_CLIENT_ID_NAME')!,
+            clientSecret: this.configService.get<string>('KEYCLOAK_CLIENT_SECRET')!,
+          });
+        } catch (error) {
+          console.error('❌ Token refresh failed', error.message);
+        }
+      }, 58 * 1000);
+
+    } catch (error) {
+      console.error('❌ Failed to authenticate Keycloak Admin Client');
+      console.error('Error:', error.responseData || error.message);
+      throw error;
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    try {
+      const user = await this.kcAdminClient.users.create({
+        realm: this.configService.get<string>('KEYCLOAK_REALM')!,
+        username: createUserDto.username,
+        email: createUserDto.email,
+        firstName: createUserDto.firstName,
+        lastName: createUserDto.lastName,
+        enabled: true,
+        emailVerified: true,
+        credentials: [{
+          type: 'password',
+          value: createUserDto.password,
+          temporary: false,
+        }],
+      });
+
+      console.log('✅ User created:', user);
+      await this.assignRoleToUser(user.id, 'Acheteur');
+      return user;
+    } catch (error) {
+      console.error('❌ Failed to create user:', error);
+      throw error;
+    }
+  }
+
+  private async assignRoleToUser(userId: string, roleName: string) {
+    try {
+      const role = await this.kcAdminClient.roles.findOneByName({
+        realm: this.configService.get<string>('KEYCLOAK_REALM')!,
+        name: roleName,
+      });
+
+      if (!role) {
+        console.warn(`⚠️ Role "${roleName}" not found in Keycloak`);
+        return;
+      }
+
+      await this.kcAdminClient.users.addRealmRoleMappings({
+        realm: this.configService.get<string>('KEYCLOAK_REALM')!,
+        id: userId,
+        roles: [{
+          id: role.id!,
+          name: role.name!,
+        }],
+      });
+
+      console.log(`✅ Role "${roleName}" assigned to user ${userId}`);
+
+    } catch (error) {
+      console.error(`❌ Failed to assign role "${roleName}":`, error);
+      throw error;
+    }
   }
 }
