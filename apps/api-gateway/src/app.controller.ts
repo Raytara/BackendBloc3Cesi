54a7818ee1e3f01587fd18f3484b152661a90dbb
Raytara <maxime.waechter@gmail.com>
2025-12-14 23:07:41 +0100

    feat: connexion keyclock

--- apps/api-gateway/src/app.controller.ts
+++ apps/api-gateway/src/app.controller.ts
@@ -1,17 +1,26 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Body, Post, Controller, Get, HttpCode, HttpStatus, Inject } from '@nestjs/common';
+import { Public } from 'nest-keycloak-connect';
 import { AppService } from './app.service';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller()
+@Controller('users')
 export class AppController {
-  constructor(@Inject('USER_SERVICE') private readonly usersClient: ClientProxy) {}
+  constructor(@Inject('USER_SERVICE') private readonly usersClient: ClientProxy) { }
 
   @Get('hello')
+  @Public()
   getHelloFromMicroservice(): Observable<string> {
     const pattern = 'hello_users_service'; 
     const payload = {}; 
 
     return this.usersClient.send<string>(pattern, payload);
+  }
+
+  @Post()
+  @Public()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersClient.send('create_user', createUserDto);
   }
 }
