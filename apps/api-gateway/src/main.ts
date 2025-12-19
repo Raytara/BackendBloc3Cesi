4a3911a88597637633d681da7c04c9e77c4bf3bc
Raytara <maxime.waechter@gmail.com>
2025-12-19 14:47:46 +0100

    fix: CORS

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origine: 'http://localhost:4000', 
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  }); 
+
   await app.listen(process.env.PORT ?? 3000);
   console.log('API Gateway is running on http://localhost:3000');
 }
